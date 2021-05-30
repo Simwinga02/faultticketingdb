92d44875983468b34f5907ca110a4382e374c625
unknown <josayasakala@gmail.com>
2021-05-30 19:34:30 +0200

    intial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f4c5f801e2c7222cb4e9975223343acf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '852837a9-738b-4133-a74b-284c6ed8c7cc'
+};
